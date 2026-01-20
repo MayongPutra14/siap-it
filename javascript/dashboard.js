0f531cbc5c2f430ae30194b4ac7a61d726b47d31
Mayong Saputra <gilang46622@gmail.com>
2026-01-20 15:25:29 +0700

    fix mockup and path

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -4,7 +4,3 @@ import "./components/active-menu.js";
 import "./components/ipk-chart.js";
 import "./components/selected-menu.js";
 import "./components/theme.js";
-
-// API
-import "./api/dashboard.js"
-import "./api/logout.js"
